4b7bf7b5247320ec9310949e3c585449dc29b006
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-22 17:03:21 +0900

    src/components/AddTodo.tsxを作成する

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,19 @@ import useSWR from 'swr';
 import './App.css';
 import AddTodo from './components/AddTodo';
 
+export interface Todo {
+  id: number;
+  title: string;
+  body: string;
+  done: boolean;
+}
+
 export const ENDPOINT = 'http://localhost:4000';
 
 const fetcher = (url: string) =>
   fetch(`${ENDPOINT}/${url}`).then((r) => r.json());
 function App() {
-  const { data, mutate } = useSWR('api/todos', fetcher);
+  const { data, mutate } = useSWR<Todo[]>('api/todos', fetcher);
   return (
     <Box>
       {JSON.stringify(data)}
