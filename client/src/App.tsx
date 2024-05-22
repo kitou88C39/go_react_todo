cfe9350e7bdf253726642638414155730361a21e
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-22 13:36:03 +0900

    client/src/App.tsxを編集する

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,13 @@ import { Box } from '@mantine/core';
 import useSWR from 'swr';
 import './App.css';
 
-const fetcher = (url: string) => fetch(url).then((r) => r.json());
+export const ENDPOINT = 'http://localhost:4000';
+
+const fetcher = (url: string) =>
+  fetch(`${ENDPOINT}/${url}`).then((r) => r.json());
 function App() {
-  return <Box></Box>;
+  const { data, mutate } = useSWR('api/todos', fetcher);
+  return <Box>{JSON.stringify(data)}</Box>;
 }
 
 export default App;
