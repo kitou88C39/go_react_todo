f66c4f61afeb75ba3d74769976658f6e5cecfb8a
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-22 14:56:48 +0900

    client/components/AddTodo.tsxを作成する

--- client/components/AddTodo.tsx
+++ client/components/AddTodo.tsx
@@ -1,5 +1,7 @@
+import React from 'react';
 import { useState } from 'react';
 import { useForm } from '@mantine/hooks';
+import { Modal } from '@mantine/core';
 
 function AddTodo() {
   const [open, setOpen] = useState(false);
@@ -10,5 +12,16 @@ function AddTodo() {
       body: '',
     },
   });
+
+  return (
+    <>
+      <Modal
+        opened={open}
+        onClose={() => setOpen(false)}
+        title='Create todo'
+      ></Modal>
+    </>
+  );
 }
+
 export default AddTodo;
